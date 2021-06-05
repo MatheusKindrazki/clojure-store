75c9f7160c683e7d1ab24db499a50e19a9584df9
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-06-04 22:21:19 -0300

    Adicionando tipos de dados

--- src/loja/core.clj
+++ src/loja/core.clj
@@ -1,6 +1,9 @@
 (ns loja.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;["daniela" "guilherme" "carlos" "paulo" "lucia" "ana"]
+;{"guilherme" 37, "paulo" 39}
+;(1 2 3 4 5)
+;[[0 1]]
+;#{}
+
+;(map println ["daniela" "guilherme" "carlos" "paulo" "lucia" "ana"])
