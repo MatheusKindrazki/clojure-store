1858cd58949bd40cd3c8654b75244da0ef99ae8c
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-06-05 18:46:44 -0300

    Estudando sobre funções eager e lazy

---
+++ src/loja/aula5.clj
@@ -0,0 +1,33 @@
+(ns loja.aula5
+  (:require [loja.db :as l.db]
+            [loja.logic :as l.logic]))
+
+(defn gastou-bastante? [info-do-usuario]
+  (> (:preco-total info-do-usuario) 500))
+
+(let [pedidos  (l.db/todos-os-pedidos)
+      resumo   (l.logic/resumo-por-usuario pedidos)]
+      (println "keep" (keep gastou-bastante? resumo))
+      (println "filter" (filter gastou-bastante? resumo)))
+
+(println "\n Tentando entender keep")
+
+(defn gastou-bastante? [info-do-usuario]
+  (println "gastou bastante" (:usuarios-id info-do-usuario))
+  (> (:preco-total info-do-usuario) 500))
+
+(let [pedidos  (l.db/todos-os-pedidos)
+      resumo   (l.logic/resumo-por-usuario pedidos)]
+  (println "keep" (keep gastou-bastante? resumo)))
+
+(println "\n Vamos isolaer")
+
+(println (range 10))
+(println (take 2(range 10000)))
+;a sequência não esta sendo "gulosa" (eager)
+
+(let [sequencia (range 1000000)]
+  (println (take 2 sequencia))
+  (println (take 2 sequencia)))
+; Função LAZY ( preguiçoso )
+
